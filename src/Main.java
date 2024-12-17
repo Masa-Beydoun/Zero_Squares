cf6238dcd0d8f5ed9578347743a6f0a68232aebc
Masa <masa.beydoun@hotmail.com>
2024-12-17 12:26:03 +0300

    more developments

--- src/Main.java
+++ src/Main.java
@@ -17,29 +17,28 @@ public class Main {
     //1 2 3 4 5 6 7 8
     public static void main(String[] args) {
 
-//        new MainFrame(7);
+        new MainFrame(30);
 
-        deleteFile("BFS");
-        deleteFile("DFS");
-        deleteFile("DFS_RECURSIVE");
-        deleteFile("aStarSearch");
-        deleteFile("SimpleHillClimbing");
-        deleteFile("SteepestHillClimbing");
-        deleteFile("UCS");
+//        deleteFile("BFS");
+//        deleteFile("DFS");
+//        deleteFile("DFS_RECURSIVE");
+//        deleteFile("aStarSearch");
+//        deleteFile("SimpleHillClimbing");
+//        deleteFile("SteepestHillClimbing");
+//        deleteFile("UCS");
 
 
         Algorithms algorithms = new Algorithms();
-        for(int i=21;i<=30;i++){
-            State state = InputStates.readGrid(i);
-            algorithms.startAlgorithm(AlgorithmName.BFS,state,i);
-            algorithms.startAlgorithm(AlgorithmName.DFS,state,i);
-            algorithms.startAlgorithm(AlgorithmName.UCS,state,i);
-            algorithms.startAlgorithm(AlgorithmName.SIMPLE_HILL_CLIMBING,state,i);
-            algorithms.startAlgorithm(AlgorithmName.STEEPEST_HILL_CLIMBING,state,i);
-            algorithms.startAlgorithm(AlgorithmName.A_STAR,state,i);
-            algorithms.startAlgorithm(AlgorithmName.A_STAR_ADVANCED,state,i);
-            algorithms.initiate_DfS(state,i);
-
-        }
+//        for(int i=1;i<=30;i++){
+//            State state = InputStates.readGrid(i);
+//            algorithms.startAlgorithm(AlgorithmName.BFS,state,i);
+//            algorithms.startAlgorithm(AlgorithmName.DFS,state,i);
+//            algorithms.startAlgorithm(AlgorithmName.UCS,state,i);
+//            algorithms.startAlgorithm(AlgorithmName.SIMPLE_HILL_CLIMBING,state,i);
+//            algorithms.startAlgorithm(AlgorithmName.STEEPEST_HILL_CLIMBING,state,i);
+//            algorithms.startAlgorithm(AlgorithmName.A_STAR,state,i);
+//            algorithms.startAlgorithm(AlgorithmName.A_STAR_ADVANCED,state,i);
+//            algorithms.initiate_DfS(state,i);
+//        }
     }
 }
